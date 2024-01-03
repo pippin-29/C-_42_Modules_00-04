d02e3bd61309cedfc1fcc2fd69e9a187769ae2c2
Daniel H <operas.referee.0e@icloud.com>
2024-01-04 09:31:27 +1030

    C++ Module 01 Update

---
+++ C++_01_1A_GH/ex00/headers/main.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhadding <operas.referee.0e@icloud.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 09:09:31 by dhadding          #+#    #+#             */
+/*   Updated: 2024/01/04 09:13:39 by dhadding         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+/// MACRO SECTION ///
+# define ERROR 1
+# define SUCCESS 0
+# define MAIN_HEADER_PATH "../headers/main.h"
+///
+
+/// INCLUDE STATEMENTS ///
+# include <iostream>
+# include "newZombie.hpp"
+# include "randomChump.hpp"
+# include "Zombie.hpp"
+///
+
+#endif
