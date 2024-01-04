0d581e51948336a597570d7a4b66bda3b1726d6b
Daniel H <operas.referee.0e@icloud.com>
2024-01-04 14:56:37 +1030

    ex03 started...

---
+++ C++_01_1A_GH/ex03/headers/main.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhadding <operas.referee.0e@icloud.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 14:51:37 by dhadding          #+#    #+#             */
+/*   Updated: 2024/01/04 14:53:43 by dhadding         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+/// MACRO SECTION ///
+# define ERROR 1
+# define SUCCESS 0
+/// ^MACRO SECTION^ ///
+
+/// INCLUDE STATEMENTS ///
+# include <iostream>
+# include "HumanA.hpp"
+# include "HumanB.hpp"
+# include "Weapon.hpp"
+/// ^INCLUDE STATEMENTS^ ///
+
+#endif
